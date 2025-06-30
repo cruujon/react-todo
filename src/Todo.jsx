55ab21d3ca5c1d99df8ad808b2e8a96106ac4b2a
cruujon.eth <huitaiheiyan3@gmail.com>
2025-06-30 10:27:31 +0000

    ああああ

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -22,25 +22,27 @@ export const Todo = () => {
     setTodoText("");
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
+        <input placeholder="TODOを入力" onChange={onChangeTodoText} />
         <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className="list-row">
                 <p className="todo-item ">{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
